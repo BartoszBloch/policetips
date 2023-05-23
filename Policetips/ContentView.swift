97e5ce71de3857aa02730e64d419e8e2d6ba03b5
Bartosz Bloch <blochbartosz1@gmail.com>
2023-05-23 10:53:38 +0200

    first commit - commiting all the files to the repo ;c

---
+++ Policetips/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Policetips
+//
+//  Created by Bartosz Bloch on 04/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading ,spacing: 20.0) {
+            
+            Image("police")
+                .resizable()
+                .padding()
+                .cornerRadius(50)
+                
+                
+            
+            
+            HStack {
+                Spacer()
+                Text("PoliceTips")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.blue)
+                    .multilineTextAlignment(.trailing)
+                    
+                    
+        
+                   
+                
+                Image(systemName: "star.circle.fill")
+                    .foregroundColor(Color.blue)
+                    .font(.largeTitle)
+                
+                Spacer()
+            }
+
+            
+           
+            
+            
+            Text("Twój błąd może kosztować życie - Twoje lub innych. Tylko profesjonalizm Cię uratuje.")
+                .font(.body)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.trailing)
+                
+                
+                
+                
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Policetips/PolicetipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolicetipsApp.swift
+//  Policetips
+//
+//  Created by Bartosz Bloch on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PolicetipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
